46bc84164fd6e885fef24597d7fa4472b4edcfb7
syed haziq <syed.haziq40@gmail.com>
2024-03-30 17:22:05 +0500

    project completed

--- dagster_orchestration/dagster_orchestration/__init__.py
+++ dagster_orchestration/dagster_orchestration/__init__.py
@@ -1,9 +1,5 @@
 from dagster import Definitions, load_assets_from_modules
 
-from . import assets
+from .assets import resources
 
-all_assets = load_assets_from_modules([assets])
-
-defs = Definitions(
-    assets=all_assets,
-)
+defs = Definitions(assets=load_assets_from_modules([assets]), resources=resources)
